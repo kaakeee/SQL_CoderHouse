5e1ca1cc07dacba129065a410f9cc6e2d212d431
Ramiro Sanabria <111094495+kaakeee@users.noreply.github.com>
2022-09-29 18:29:02 -0300

    Update Final_work.sql

--- Final_work.sql
+++ Final_work.sql
@@ -1,4 +1,8 @@
-------------------------------------------------------------------------------------------------------------------------------------
+####################################################################################################################
+####################################################Data Base#######################################################
+####################################################################################################################
+
+DROP DATABASE pyme_drop_shipping;
 CREATE DATABASE IF NOT EXISTS  PYME_DROP_SHIPPING;
 USE PYME_DROP_SHIPPING;
 /*DER https://drive.google.com/file/d/1IAkyLe8vKdq37fqXS9MOKFmY8whUt86w/view?usp=sharing */
@@ -74,8 +78,28 @@ condition_purchase int,
 Foreign key (id_order) REFERENCES Orders (id_order)
 );
 
-#Data
-------------------------------------------------------------------------------------------------------------------------------------
+################################Table for Triggers#########################
+CREATE TABLE IF NOT EXISTS old_billing (
+id_billing INT,  
+tax_number VARCHAR(40),
+total_purchase DECIMAL(30,3),
+id_order INT,
+user CHAR(30),     
+date_hour timestamp
+);
+
+/*new sales record table, from orders table*/ 
+
+CREATE TABLE IF NOT EXISTS new_orders(
+id_order INT,
+amount DECIMAL(20,2),
+user CHAR(30),    
+date_hour timestamp
+);
+
+####################################################################################################################
+####################################################Data############################################################
+####################################################################################################################
 /*insert test data*/
 insert into Customers (customer_id, first_name, last_name, phone, email) 
 values  
@@ -167,8 +191,9 @@ values
 ('deacfd6b00beea2fbb67e2c7a54abae8b02c7740', 9, '49150 Muir Lane', 'Mexico','2'),
 ('bdac11b1aa117086f7d9f6ef51283539accb093a', 10, '9 Duke Parkway', 'Solomon Islands','1');
 
-#Store Procedure
-------------------------------------------------------------------------------------------------------------------------------------
+####################################################################################################################
+####################################################Store Procedure#################################################
+####################################################################################################################
 /* in the column field put some column from the orders table, and set the order_by form, ie 'ASC' or 'DESC'*/
 DELIMITER //
 CREATE PROCEDURE `search_Orders` (IN column_a VARCHAR(20), IN order_by VARCHAR(4))
@@ -223,9 +248,9 @@ DEALLOCATE PREPARE resultSQL;
 
 END //
 
-#Views
-------------------------------------------------------------------------------------------------------------------------------------
-/* 5 views*/
+####################################################################################################################
+####################################################Views###########################################################
+####################################################################################################################
 
 /*Premiums Customers*/
 CREATE OR REPLACE VIEW v_Total_Buy_premium_customers
@@ -291,8 +316,9 @@ order by total_purchase desc
 );
 SELECT * FROM PYME_DROP_SHIPPING.v_best_seller;
 
-------------------------------------------------------------------------------------------------------------------------------------
-#Users
+####################################################################################################################
+####################################################Users###########################################################
+####################################################################################################################
 
 CREATE USER 'Data_Analist'@'localhost' IDENTIFIED BY 'iLoveData32';
 CREATE USER 'IT'@'localhost' IDENTIFIED BY 'IamADM68k';                /*users and passwords*/
@@ -302,12 +328,162 @@ GRANT SELECT ON *.* TO 'Data_Analist'@'localhost';                     /*just re
 GRANT SELECT, UPDATE, INSERT ON *.* TO 'IT'@'localhost';               /*read, modify and create*/
 GRANT ALL ON *.* TO 'Boss'@'localhost';                               /*all grant*/
 
-/* DROP USER 'IT'@'localhost'; */ /*PARA ELIMINARLOS*/
-/* DROP USER 'Data_Analist'@'localhost'; */
-/* DROP USER 'Boss'@'localhost'; */
+/* 
+GRANT SELECT ON pyme_drop_shipping.billing TO 'Data_Analist'@'localhost';
+DROP USER 'IT'@'localhost'; 
+DROP USER 'Data_Analist'@'localhost'; 
+DROP USER 'Boss'@'localhost';
+DROP DATABASE pyme_drop_shipping;
+ALTER USER 'Boss'@'localhost' identified by 'newpass,notsecure';
+RENAME USER 'Boss'@'localhost' TO 'MegaBoss'@'localhost';
+
+ */
 
 SELECT * FROM mysql.user WHERE user LIKE 'IT%';               /*to check your permissions*/
 SELECT * FROM mysql.user WHERE user LIKE 'Data_Analist%';   
 SELECT * FROM mysql.user WHERE user LIKE 'Boss%';              
 
-------------------------------------------------------------------------------------------------------------------------------------
+
+####################################################################################################################
+##################################################Triggers##########################################################
+####################################################################################################################
+
+/*I create tables to have a follow-up with the triggers*/
+/*log table of new invoices with their amounts and users who created the table and modifications*/
+
+
+CREATE TRIGGER tr_billing
+AFTER update ON billing                                         /*who do you expect to be updated to shoot*/
+FOR EACH ROW                                                   /*This means that for each row that the trigger updates, it acts*/
+INSERT INTO old_billing (id_billing, tax_number, total_purchase, id_order, user, date_hour)  /*copy*/
+VALUES (old.id_billing, old.tax_number, old.total_purchase, old.id_order, session_user(), current_timestamp());   /*original*/
+
+
+/*this table is in charge of seeing who enters the new orders and who does it*/
+
+CREATE TRIGGER tr_orders
+BEFORE INSERT on orders
+FOR EACH ROW
+INSERT INTO new_orders (id_order, amount, user, date_hour)   
+VALUES (NEW.id_order, NEW.amount, session_user(), current_timestamp());  
+
+ 
+/*
+DROP TRIGGER tr_billing;
+DROP TRIGGEr tr_orders; 
+DROP DATABASE pyme_drop_shipping;
+*/
+
+/* example values */
+
+
+UPDATE pyme_drop_shipping.billing SET total_purchase = '9' WHERE (id_billing = '1');
+UPDATE pyme_drop_shipping.billing SET total_purchase = '999' WHERE (id_billing = '2');
+UPDATE pyme_drop_shipping.billing SET total_purchase = '999' WHERE (id_billing = '3');
+
+insert into Orders (id_order, amount, id_model, customer_id)
+ values
+(11, '94', 5, 1),
+(12, '35', 2, 3),
+(13, '84', 6, 4),
+(14, '62', 2, 5),
+(15, '37', 9, 2),
+(16, '74', 1, 1);
+
+/*verify that the triggers work*/
+SELECT * FROM old_billing;
+SELECT * FROM new_orders;
+
+
+
+####################################################################################################################
+##################################################Funcions##########################################################
+####################################################################################################################
+
+/*search tracking number with id_order*/
+
+DELIMITER //
+CREATE FUNCTION Search_Tracking_number(id1_order INT)
+RETURNS varchar(90)
+DETERMINISTIC
+BEGIN
+DECLARE number_order VARCHAR(90);
+SET number_order = '';
+SELECT tracking_number INTO number_order
+FROM shipments
+WHERE id1_order = id_order;
+
+RETURN number_order;
+END //
+select pyme_drop_shipping.Search_Tracking_number(3);
+
+
+/*calculate the tax*/
+
+DELIMITER //
+Create FUNCTION Calculate_tax(o1rder INT) 
+RETURNS decimal(20,3)
+DETERMINISTIC
+BEGIN
+
+    DECLARE result DECIMAL(15,2);
+    DECLARE tax DECIMAL(15,2);
+    DECLARE tax_total DECIMAL(15,2);
+    SET tax = 0.21;                       /*0.21 only tax or 1.21 tax + purchase*/
+   
+
+    SELECT total_purchase INTO tax_total
+    FROM billing
+    WHERE o1rder = id_order;
+    
+    SET result = tax_total * tax;
+
+
+    RETURN result;
+END //
+
+select pyme_drop_shipping.Calculate_tax(2);
+
+####################################################################################################################
+##################################################TCL###############################################################
+####################################################################################################################
+
+/*
+id_billing INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+type varchar(10),
+tax_number varchar(60),
+total_purchase DECIMAL(12,2),
+id_order INT NOT NULL,
+FOREIGN KEY (id_order) REFERENCES Orders (id_order)
+SELECT * FROM pyme_drop_shipping.billing;  
+start transaction;                        #start mode secure
+insert into billing (id_billing, type, tax_number, total_purchase, id_order) VALUES (11, 'A', 9915, 8974, 1);
+insert into billing (id_billing, type, tax_number, total_purchase, id_order) VALUES (12, 'C', 648, 90789, 2);        #test data
+insert into billing (id_billing, type, tax_number, total_purchase, id_order) VALUES (13, 'B', 5486, 546984, 3);
+insert into billing (id_billing, type, tax_number, total_purchase, id_order) VALUES (14, 'B', 457568, 4569, 4);
+savepoint billing_4;                      #1 savepoint
+ 
+insert into billing (id_billing, type, tax_number, total_purchase, id_order) VALUES (15, 'G', 98, 24289, 2);
+insert into billing (id_billing, type, tax_number, total_purchase, id_order) VALUES (16, 'A', 88, 289, 2);           #test data
+insert into billing (id_billing, type, tax_number, total_purchase, id_order) VALUES (17, 'C', 99, 76789, 2);
+insert into billing (id_billing, type, tax_number, total_purchase, id_order) VALUES (18, 'C', 6998, 8964, 2);
+savepoint billing_8;                      #2 savepoint
+SELECT * FROM pyme_drop_shipping.billing;  #SHOW CHANGES
+#RELEASE savepoint billing_4;             #delete 1 savapoint
+   rollback;                                #null changes
+   commit;                                  #apply changes
+
+
+SELECT * FROM pyme_drop_shipping.new_orders;                    #sample before changes
+start transaction;                                              #start "safe" mode
+DELETE FROM pyme_drop_shipping.new_orders WHERE id_order = 11; 
+DELETE FROM pyme_drop_shipping.new_orders WHERE id_order = 12;
+DELETE FROM pyme_drop_shipping.new_orders WHERE id_order = 13;
+DELETE FROM pyme_drop_shipping.new_orders WHERE id_order = 14;
+
+SELECT * FROM pyme_drop_shipping.new_orders;                    #show changes
+rollback;                                                       #null changes
+commit;                                                         #apply changes
+
+
+*/
