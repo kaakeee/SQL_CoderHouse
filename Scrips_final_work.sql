c9d57cb2a3919d5901028b464fa7d6d9ca552a39
Ramiro Sanabria <111094495+kaakeee@users.noreply.github.com>
2022-10-10 19:47:37 -0300

    Update Scrips_final_work.sql

--- Scrips_final_work.sql
+++ Scrips_final_work.sql
@@ -1,4 +1,45 @@
 /*
+Triggers
+I create tables to have a follow-up with the triggers
+log table of new invoices with their amounts and users who created the table and modifications*/
+
+CREATE TRIGGER tr_billing 
+AFTER update ON billing                       
+FOR EACH ROW
+INSERT INTO old_billing (id_billing, tax_number, total_purchase, id_order, user, date_hour)  
+VALUES (old.id_billing, old.tax_number, old.total_purchase, old.id_order, session_user(), current_timestamp());  
+
+
+/*this table is in charge of seeing who enters the new orders and who does it*/
+
+CREATE TRIGGER tr_orders
+BEFORE INSERT on orders
+FOR EACH ROW
+INSERT INTO new_orders (id_order, amount, user, date_hour)   
+VALUES (NEW.id_order, NEW.amount, session_user(), current_timestamp());
+
+/* example values */
+UPDATE pyme_drop_shipping.billing SET total_purchase = '9' WHERE (id_billing = '1');
+UPDATE pyme_drop_shipping.billing SET total_purchase = '999' WHERE (id_billing = '2');
+UPDATE pyme_drop_shipping.billing SET total_purchase = '999' WHERE (id_billing = '3');
+
+insert into Orders (id_order, amount, id_model, customer_id)
+ values
+(11, '94', 5, 1),
+(12, '35', 2, 3),
+(13, '84', 6, 4),
+(14, '62', 2, 5),
+(15, '37', 9, 2),
+(16, '74', 1, 1);
+
+/*verify that the triggers work*/
+SELECT * FROM old_billing;
+SELECT * FROM new_orders;
+
+
+
+
+/*
 Store Procedure
 
 in the column field put some column from the orders table, and set the order_by form, ie 'ASC' or 'DESC'
@@ -58,90 +99,13 @@ DEALLOCATE PREPARE resultSQL;
 END //
 
 
-/*
-Users
-*/
-
-#CREATE USER 'Data_Analist'@'localhost' IDENTIFIED BY 'iLoveData32';
-#CREATE USER 'IT'@'localhost' IDENTIFIED BY 'IamADM68k';                /*users and passwords*/
-#CREATE USER 'Boss'@'localhost' IDENTIFIED BY 'MeGaPlusUwU+OwO';
-
-#GRANT SELECT ON *.* TO 'Data_Analist'@'localhost';                     /*just read*/
-#GRANT SELECT, UPDATE, INSERT ON *.* TO 'IT'@'localhost';               /*read, modify and create*/
-#GRANT ALL ON *.* TO 'Boss'@'localhost';                               /*all grant*/
-
-/* 
-GRANT SELECT ON pyme_drop_shipping.billing TO 'Data_Analist'@'localhost';
-DROP USER 'IT'@'localhost'; 
-DROP USER 'Data_Analist'@'localhost'; 
-DROP USER 'Boss'@'localhost';
-DROP DATABASE pyme_drop_shipping;
-ALTER USER 'Boss'@'localhost' identified by 'newpass,notsecure';
-RENAME USER 'Boss'@'localhost' TO 'MegaBoss'@'localhost';
-
- */
-
-#SELECT * FROM mysql.user WHERE user LIKE 'IT%';               /*to check your permissions*/
-#SELECT * FROM mysql.user WHERE user LIKE 'Data_Analist%';   
-#SELECT * FROM mysql.user WHERE user LIKE 'Boss%';              
-
-/*
-Triggers
-*/
-
-/*I create tables to have a follow-up with the triggers*/
-/*log table of new invoices with their amounts and users who created the table and modifications*/
-
-
-CREATE TRIGGER tr_billing
-AFTER update ON billing                                         /*who do you expect to be updated to shoot*/
-FOR EACH ROW                                                   /*This means that for each row that the trigger updates, it acts*/
-INSERT INTO old_billing (id_billing, tax_number, total_purchase, id_order, user, date_hour)  /*copy*/
-VALUES (old.id_billing, old.tax_number, old.total_purchase, old.id_order, session_user(), current_timestamp());   /*original*/
-
-
-/*this table is in charge of seeing who enters the new orders and who does it*/
-
-CREATE TRIGGER tr_orders
-BEFORE INSERT on orders
-FOR EACH ROW
-INSERT INTO new_orders (id_order, amount, user, date_hour)   
-VALUES (NEW.id_order, NEW.amount, session_user(), current_timestamp());  
-
  
-/*
-DROP TRIGGER tr_billing;
-DROP TRIGGEr tr_orders; 
-DROP DATABASE pyme_drop_shipping;
-*/
-
-/* example values */
-
-
-UPDATE pyme_drop_shipping.billing SET total_purchase = '9' WHERE (id_billing = '1');
-UPDATE pyme_drop_shipping.billing SET total_purchase = '999' WHERE (id_billing = '2');
-UPDATE pyme_drop_shipping.billing SET total_purchase = '999' WHERE (id_billing = '3');
-
-insert into Orders (id_order, amount, id_model, customer_id)
- values
-(11, '94', 5, 1),
-(12, '35', 2, 3),
-(13, '84', 6, 4),
-(14, '62', 2, 5),
-(15, '37', 9, 2),
-(16, '74', 1, 1);
-
-/*verify that the triggers work*/
-SELECT * FROM old_billing;
-SELECT * FROM new_orders;
 
 /*
 
 Funcions
 
-*/
-
-/*search tracking number with id_order*/
+search tracking number with id_order*/
 
 DELIMITER //
 CREATE FUNCTION Search_Tracking_number(id1_order INT)
@@ -156,12 +120,10 @@ WHERE id1_order = id_order;
 
 RETURN number_order;
 END //
-select pyme_drop_shipping.Search_Tracking_number(3);
 
-
-/*calculate the tax*/
 
 DELIMITER //
+/*calculate the tax*/
 Create FUNCTION Calculate_tax(o1rder INT) 
 RETURNS decimal(20,3)
 DETERMINISTIC
@@ -183,50 +145,9 @@ BEGIN
     RETURN result;
 END //
 
+
 select pyme_drop_shipping.Calculate_tax(2);
-
-/*
-
-TCL
-
-*/
-
-/*
-id_billing INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
-type varchar(10),
-tax_number varchar(60),
-total_purchase DECIMAL(12,2),
-id_order INT NOT NULL,
-FOREIGN KEY (id_order) REFERENCES Orders (id_order)
-SELECT * FROM pyme_drop_shipping.billing;  
-start transaction;                        #start mode secure
-insert into billing (id_billing, type, tax_number, total_purchase, id_order) VALUES (11, 'A', 9915, 8974, 1);
-insert into billing (id_billing, type, tax_number, total_purchase, id_order) VALUES (12, 'C', 648, 90789, 2);        #test data
-insert into billing (id_billing, type, tax_number, total_purchase, id_order) VALUES (13, 'B', 5486, 546984, 3);
-insert into billing (id_billing, type, tax_number, total_purchase, id_order) VALUES (14, 'B', 457568, 4569, 4);
-savepoint billing_4;                      #1 savepoint
- 
-insert into billing (id_billing, type, tax_number, total_purchase, id_order) VALUES (15, 'G', 98, 24289, 2);
-insert into billing (id_billing, type, tax_number, total_purchase, id_order) VALUES (16, 'A', 88, 289, 2);           #test data
-insert into billing (id_billing, type, tax_number, total_purchase, id_order) VALUES (17, 'C', 99, 76789, 2);
-insert into billing (id_billing, type, tax_number, total_purchase, id_order) VALUES (18, 'C', 6998, 8964, 2);
-savepoint billing_8;                      #2 savepoint
-SELECT * FROM pyme_drop_shipping.billing;  #SHOW CHANGES
-#RELEASE savepoint billing_4;             #delete 1 savapoint
-   rollback;                                #null changes
-   commit;                                  #apply changes
+select pyme_drop_shipping.Search_Tracking_number(3);
 
 
-SELECT * FROM pyme_drop_shipping.new_orders;                    #sample before changes
-start transaction;                                              #start "safe" mode
-DELETE FROM pyme_drop_shipping.new_orders WHERE id_order = 11; 
-DELETE FROM pyme_drop_shipping.new_orders WHERE id_order = 12;
-DELETE FROM pyme_drop_shipping.new_orders WHERE id_order = 13;
-DELETE FROM pyme_drop_shipping.new_orders WHERE id_order = 14;
-
-SELECT * FROM pyme_drop_shipping.new_orders;                    #show changes
-rollback;                                                       #null changes
-commit;                                                         #apply changes
-
-
-*/
+            
